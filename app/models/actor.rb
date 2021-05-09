fc181a3c9c5660e0a159c7cd24286d42460da94d
johnson-cole <colejohnson@chicagobooth.edu>
2021-05-09 16:01:08 +0000

    annotated models and working on associations

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: actors
+#
+#  id         :bigint           not null, primary key
+#  bio        :text
+#  dob        :date
+#  image      :string
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Actor < ApplicationRecord
+  belongs_to :movie
+
+  has_many :characters, through: :movie, dependent: :destroy
+  has_many :movies, dependent: :destroy
 end
--- app/models/director.rb
+++ app/models/director.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: directors
+#
+#  id         :bigint           not null, primary key
+#  bio        :text
+#  dob        :date
+#  image      :string
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Director < ApplicationRecord
+  has_many :movies, dependent: :destroy 
+  
 end
